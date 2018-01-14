0a32ed9457ddbf687cb61c57440462c8b64740e4
Dan Katzuv <student@Robotics.k12.il>
2018-01-14 21:05:26 +0200

    Add climb command

--- src/org/usfirst/frc/team5987/robot/commands/ClimbCommand.java
+++ src/org/usfirst/frc/team5987/robot/commands/ClimbCommand.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team5987.robot.commands;
 
+import org.usfirst.frc.team5987.robot.Robot;
+import org.usfirst.frc.team5987.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -10,14 +13,17 @@ public class ClimbCommand extends Command {
     public ClimbCommand() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Robot.climb);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.climb.setServos(RobotMap.destination);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.climb.setClimbSpeed(RobotMap.climbSpeed);
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -27,6 +33,7 @@ public class ClimbCommand extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.climb.setClimbSpeed(0);
     }
 
     // Called when another command which requires one or more of the same
