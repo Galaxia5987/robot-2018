40cda396be74389e650f893a072c40f00c08b035
msmow <student@Robotics.k12.il>
2018-01-14 21:15:43 +0200

    Created LiftSubsystem
    Currently using depracated NetworkTables

--- src/org/usfirst/frc/team5987/robot/RobotMap.java
+++ src/org/usfirst/frc/team5987/robot/RobotMap.java
@@ -14,13 +14,11 @@ package org.usfirst.frc.team5987.robot;
  * floating around.
  */
 public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
+	
+	public static int liftMotorPort = 0;
+	public static int liftEncoderPortA = 0;
+	public static int liftEncoderPortB = 0;
+	public static int liftHallEffect1Port = 0;
+	public static int liftHallEffect2Port = 0;
+	
 }
---
+++ src/org/usfirst/frc/team5987/robot/subsystems/LiftSubsystem.java
@@ -0,0 +1,59 @@
+package org.usfirst.frc.team5987.robot.subsystems;
+
+import org.usfirst.frc.team5987.robot.RobotMap;
+
+import auxiliary.MiniPID;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+
+/**
+ *
+ */
+public class LiftSubsystem extends Subsystem {
+	
+	NetworkTable LiftTable = NetworkTable.getTable("liftTable");
+	Victor liftMotor = new Victor(RobotMap.liftMotorPort);
+	Encoder liftEncoder = new Encoder(RobotMap.liftEncoderPortA, RobotMap.liftEncoderPortB);
+	DigitalInput hallEffect1 = new DigitalInput(RobotMap.liftHallEffect1Port);
+	DigitalInput hallEffect2 = new DigitalInput(RobotMap.liftHallEffect2Port);
+	double wantedHeight;
+
+    public void initDefaultCommand() {
+    }
+    
+    public void setSpeed(double speed) {
+    	liftMotor.set(speed);
+    }
+    
+    public double getPosition() {
+    	return liftEncoder.getDistance();
+    }
+    
+    public double getSpeed() {
+    	return liftEncoder.getRate();
+    }
+    
+    public boolean isUp() {
+    	return hallEffect1.get();
+    }
+    
+    public boolean isDown() {
+    	return hallEffect2.get();
+    }
+    
+    public void setPoint(double height) {
+    	wantedHeight = height;
+    }
+    
+    public void Update() {
+    	MiniPID pid = new MiniPID(LiftTable.getNumber("liftP", 0), LiftTable.getNumber("liftI", 0), LiftTable.getNumber("liftD", 0));
+    	pid.setOutputLimits(0, 1);
+    	pid.setSetpoint(wantedHeight);
+    	setSpeed(pid.getOutput());
+    }
+}
+
