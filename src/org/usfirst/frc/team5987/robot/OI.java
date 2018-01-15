fb791b2436ac4f899974a55f2400c4f2e1926a75
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-01-15 20:20:31 +0200

    Add xbox buttons

--- src/org/usfirst/frc/team5987/robot/OI.java
+++ src/org/usfirst/frc/team5987/robot/OI.java
@@ -7,6 +7,14 @@
 
 package org.usfirst.frc.team5987.robot;
 
+import org.usfirst.frc.team5987.robot.commands.ClimbCommand;
+import org.usfirst.frc.team5987.robot.commands.OpenHooksCommand;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
@@ -19,7 +27,10 @@ public class OI {
 	// number it is.
 	// Joystick stick = new Joystick(port);
 	// Button button = new JoystickButton(stick, buttonNumber);
-
+	XboxController xbox = new XboxController(0);
+	Button a = new JoystickButton(xbox, 1);
+	Button b = new JoystickButton(xbox, 2);
+	Button x = new JoystickButton(xbox, 3);
 	// There are a few additional built in buttons you can use. Additionally,
 	// by subclassing Button you can create custom triggers and bind those to
 	// commands the same as any other Button.
@@ -39,4 +50,9 @@ public class OI {
 	// Start the command when the button is released and let it run the command
 	// until it is finished as determined by it's isFinished method.
 	// button.whenReleased(new ExampleCommand());
+	public OI() {
+		a.whenPressed(new ClimbCommand());
+		b.whenPressed(new OpenHooksCommand(true));
+		x.whenPressed(new OpenHooksCommand(false));
+	}
 }
