b6db358e4c414b4a0345264788929e100b7ade4d
Dan Katzuv <student@Robotics.k12.il>
2018-01-15 17:03:54 +0200

    Add command for openning the hooks

---
+++ src/org/usfirst/frc/team5987/robot/commands/OpenHooksCommand.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team5987.robot.commands;
+
+import org.usfirst.frc.team5987.robot.Robot;
+import org.usfirst.frc.team5987.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class OpenHooksCommand extends Command {
+
+	public OpenHooksCommand() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.climb);
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		Robot.climb.setServos(RobotMap.destination);
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return true;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
+}
