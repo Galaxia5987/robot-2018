57a061d5d083a4a919bd675b19e63b6ac3124d15
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-01-15 21:29:54 +0200

    changed to dry-test ports
    for:
    2 servos, vicor and limitswitch of climb

--- src/org/usfirst/frc/team5987/robot/RobotMap.java
+++ src/org/usfirst/frc/team5987/robot/RobotMap.java
@@ -16,13 +16,12 @@ package org.usfirst.frc.team5987.robot;
 public class RobotMap {
 	
 	/*--------------------------CLIMB-------------------------------*/
-	public static int servo1 = 8; // TODO: CHANGE!
-	public static int servo2 = 9; // TODO: CHANGE!
-	public static int motor = 2; // TODO: CHANGE!
-	public static int limitSwitch = 9; // TODO: CHANGE!
-	public static double climbSpeed = 0.3;
-	public static double openPosition = 1;
-	public static double closedPosition = 0;
+	public static int climbRightServo = 8; // TODO: CHANGE!
+	public static int climbLeftServo = 9; // TODO: CHANGE!
+	public static int climbMotor = 3; // TODO: CHANGE!
+	public static int climbLimitSwitch = 9; // TODO: CHANGE!
+	public static double climbServoOpenPosition = 1;
+	public static double climbServoClosedPosition = 0;
 	/*--------------------------------------------------------------*/
 
 	
