14f758497a8a48539aecdccbed7ef76f31889ace
Dor Brekhman <dor46200@gmail.com>
2018-01-16 17:46:22 +0200

    add docs (by dor :) )

--- src/org/usfirst/frc/team5987/robot/subsystems/intakeSubsystem.java
+++ src/org/usfirst/frc/team5987/robot/subsystems/intakeSubsystem.java
@@ -17,12 +17,21 @@ public class intakeSubsystem extends Subsystem {
 	private Victor intakeVictorRight=new Victor(RobotMap.intakeMotorRight);
 	private DoubleSolenoid solenoid=new DoubleSolenoid(RobotMap.intakeSolenoid1,RobotMap.intakeSolenoid2);
 	
+	/**
+	 * Set the speed for the intake wheels
+	 * @param speedLeft - speed between -1 and 1
+	 * @param speedRight - speed between -1 and 1
+	 */
 	public void setSpeed(double speedLeft, double speedRight) {
 		// Basic function to set motor speed
 		intakeVictorLeft.set(speedLeft);
 		intakeVictorRight.set(speedRight);
 	}
 	
+	/**
+	 * Open the intake mechanism (expends beyond the frame perimeter) <br>
+	 * @param open - true if you want to open the mechanism, false to close
+	 */
 	public void setSolenoid(boolean open) {
 		//open decides weather to open the piston or close it
 		if(open) {
