e27dba86cead795c368342b35ddc4d26f469c14b
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-01-17 22:31:30 +0200

    Commit

--- src/org/usfirst/frc/team5987/robot/subsystems/intakeSubsystem.java
+++ src/org/usfirst/frc/team5987/robot/subsystems/intakeSubsystem.java
@@ -7,43 +7,47 @@ import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- *@author ACoolName
- *@version 1.0
+ * @author ACoolName
+ * @version 1.0
  */
-public class intakeSubsystem extends Subsystem {
+public class IntakeSubsystem extends Subsystem {
 
-    //Defining motors and piston
-	private Victor intakeVictorLeft=new Victor(RobotMap.intakeMotorLeft);
-	private Victor intakeVictorRight=new Victor(RobotMap.intakeMotorRight);
-	private DoubleSolenoid solenoid=new DoubleSolenoid(RobotMap.intakeSolenoid1,RobotMap.intakeSolenoid2);
-	
+	// Defining motors and piston
+	private Victor intakeMotorLeft = new Victor(RobotMap.intakeMotorLeft);
+	private Victor intakeMotorRight = new Victor(RobotMap.intakeMotorRight);
+	private DoubleSolenoid solenoid = new DoubleSolenoid(RobotMap.intakeSolenoid1, RobotMap.intakeSolenoid2);
+
 	/**
 	 * Set the speed for the intake wheels
-	 * @param speedLeft - speed between -1 and 1
-	 * @param speedRight - speed between -1 and 1
+	 * 
+	 * @param speedLeft
+	 *            - speed between -1 and 1
+	 * @param speedRight
+	 *            - speed between -1 and 1
 	 */
 	public void setSpeed(double speedLeft, double speedRight) {
 		// Basic function to set motor speed
-		intakeVictorLeft.set(speedLeft);
-		intakeVictorRight.set(speedRight);
+		intakeMotorLeft.set(speedLeft);
+		intakeMotorRight.set(speedRight);
 	}
-	
+
 	/**
-	 * Open the intake mechanism (expends beyond the frame perimeter) <br>
-	 * @param open - true if you want to open the mechanism, false to close
+	 * Open the intake mechanism (expends beyond the frame perimeter)
+	 * 
+	 * @param open
+	 *            - Whether you want to open the mechanism.
 	 */
 	public void setSolenoid(boolean open) {
-		//open decides weather to open the piston or close it
-		if(open) {
+		// open decides weather to open the piston or close it
+		if (open) {
 			solenoid.set(DoubleSolenoid.Value.kForward);
-		}else {
+		} else {
 			solenoid.set(DoubleSolenoid.Value.kReverse);
 		}
 	}
-	
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
-}
 
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+}
