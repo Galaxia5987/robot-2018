75d7ddcbbf82be53f520c0fbe8631ac34022d4a4
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-01-18 22:21:18 +0200

    Add differential drive and speed controllers

--- src/org/usfirst/frc/team5987/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team5987/robot/subsystems/DriveSubsystem.java
@@ -1,50 +1,54 @@
 package org.usfirst.frc.team5987.robot.subsystems;
 
 import org.usfirst.frc.team5987.robot.RobotMap;
+import org.usfirst.frc.team5987.robot.commands.JoystickDriveCommand;
 
 import auxiliary.MiniPID;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
- *@author Dor Brekhman
+ * @author Dor Brekhman
  */
 public class DriveSubsystem extends Subsystem {
-	
+
 	// PIDF constants for controlling velocity for wheels
-	private static double kP = 0; 
-	private static double kI = 0; 
+	private static double kP = 0;
+	private static double kI = 0;
 	private static double kD = 0;
 	private static double kF = 0;
 	/**
 	 * Mapping between 0-5V to METER for the analog input
 	 */
 	private static final double ultransonicMeterFactor = 1.024;
-	
-	private static final boolean rightInverted = false; // inverts the right motors & right encoder
-	private static final boolean leftInverted = true; // inverts the left motors & left encoder
-	
-	private static final Victor driveRightRearMotor = new Victor(RobotMap.driveRightRearMotor);
-	private static final Victor driveRightFrontMotor = new Victor(RobotMap.driveRightFrontMotor);
-	private static final Victor driveLeftRearMotor = new Victor(RobotMap.driveLeftRearMotor);
-	private static final Victor driveLeftFrontMotor = new Victor(RobotMap.driveLeftFrontMotor);
-	
-	private static final Encoder driveRightEncoder = new Encoder(RobotMap.driveRightEncoderChannelA, RobotMap.driveRightEncoderChannelB, rightInverted);
-	private static final Encoder driveLeftEncoder = new Encoder(RobotMap.driveLeftEncoderChannelA, RobotMap.driveLeftEncoderChannelB, leftInverted);
-	
+
+	private static final boolean rightInverted = false; // inverts the right
+														// motors & right
+														// encoder
+	private static final boolean leftInverted = true; // inverts the left motors
+														// & left encoder
+
+	private static final Victor leftFrontMotor = new Victor(RobotMap.driveLeftFrontMotor);
+	private static final Victor leftRearMotor = new Victor(RobotMap.driveLeftRearMotor);
+	private static final Victor rightFrontMotor = new Victor(RobotMap.driveRightFrontMotor);
+	private static final Victor rightRearMotor = new Victor(RobotMap.driveRightRearMotor);
+
+	private static final Encoder rightEncoder = new Encoder(RobotMap.driveRightEncoderChannelA,
+			RobotMap.driveRightEncoderChannelB, rightInverted);
+	private static final Encoder leftEncoder = new Encoder(RobotMap.driveLeftEncoderChannelA,
+			RobotMap.driveLeftEncoderChannelB, leftInverted);
+
 	private static final DigitalInput bumpSensor = new DigitalInput(RobotMap.bumpSensor);
 	/**
 	 * HRLV-MaxSonar -EZ ultrasonic sensor
 	 */
 	private static final AnalogInput backDistanceSensor = new AnalogInput(RobotMap.backUltrasonic);
-	
+
 	// Creates a new NetworkTable
 	public NetworkTable driveTable = NetworkTableInstance.getDefault().getTable("Drive");
 	// NT PIDF constants
@@ -55,156 +59,176 @@ public class DriveSubsystem extends Subsystem {
 	// NT error for debugging PIDF constants
 	NetworkTableEntry ntRightError = driveTable.getEntry("Right Speed Error");
 	NetworkTableEntry ntLeftError = driveTable.getEntry("Left Speed Error");
-	
+	public Object ;
+
 	private static MiniPID rightPID;
 	private static MiniPID leftPID;
-	
-	/*TODO Set distance per pulse TODO*/
-	public DriveSubsystem(){
-		// invert the motors if needed
-		driveRightRearMotor.setInverted(rightInverted);
-		driveRightFrontMotor.setInverted(rightInverted);
-		driveLeftRearMotor.setInverted(leftInverted);
-		driveLeftFrontMotor.setInverted(leftInverted);
 
-		driveRightFrontMotor.setInverted(rightInverted);
-		driveLeftRearMotor.setInverted(leftInverted);
-		driveLeftFrontMotor.setInverted(leftInverted);
-		// set the distance per pulse for the encoders
-		driveRightEncoder.setDistancePerPulse(RobotMap.driveEncoderDistancePerPulse);
-		driveLeftEncoder.setDistancePerPulse(RobotMap.driveEncoderDistancePerPulse);
-		
-		// init the PIDF constants in the NetworkTable
+	/* TODO Set distance per pulse TODO */
+	public DriveSubsystem() {
+		// Invert the motors if needed
+		leftFrontMotor.setInverted(leftInverted);
+		leftRearMotor.setInverted(leftInverted);
+		rightFrontMotor.setInverted(rightInverted);
+		rightRearMotor.setInverted(rightInverted);
+		// Set the distance per pulse for the encoders
+		rightEncoder.setDistancePerPulse(RobotMap.driveEncoderDistancePerPulse);
+		leftEncoder.setDistancePerPulse(RobotMap.driveEncoderDistancePerPulse);
+
+		// Set DiffrentialDrive
+		SpeedControllerGroup leftMotors = new SpeedControllerGroup(leftFrontMotor, leftRearMotor);
+		SpeedControllerGroup rightMotors = new SpeedControllerGroup(rightFrontMotor, rightRearMotor);
+		static DifferentialDrive differentialDrive = new DifferentialDrive(leftMotors, rightMotors);
+		// Initialize the PIDF constants in the NetworkTable
 		ntGetPID();
 		ntKp.setDouble(kP);
 		ntKi.setDouble(kI);
 		ntKd.setDouble(kD);
 		ntKf.setDouble(kF);
-		
-		// init the MiniPID for each side
+
+		// Initialize the MiniPID for each side
 		rightPID = new MiniPID(kP, kI, kD, kF);
 		leftPID = new MiniPID(kP, kI, kD, kF);
 	}
-	
-	/*TODO ADD DriveJoystickCommand TODO*/
+
+	/* TODO ADD DriveJoystickCommand TODO */
 	public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
-	
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+		setDefaultCommand(new JoystickDriveCommand());
+	}
+
 	/**
 	 * Gets the PIDF constants from the NetworkTable
 	 */
-	private void ntGetPID(){
+	private void ntGetPID() {
 		kP = ntKp.getDouble(kP);
 		kI = ntKi.getDouble(kI);
 		kD = ntKd.getDouble(kD);
 		kF = ntKf.getDouble(kF);
 	}
-	
+
 	/**
 	 * Updates the PIDF control and moves the motors <br>
-	 * Controls the velocity according to <code>setRightSetpoint(..)</code> and <code>setLeftSetpoint(..)</code> <br>
+	 * Controls the velocity according to <code>setRightSetpoint(..)</code> and
+	 * <code>setLeftSetpoint(..)</code> <br>
 	 * <b>This should be run periodically in order to work!</b>
 	 */
-	private void updatePID(){
+	private void updatePID() {
 		ntGetPID();
-		
+
 		rightPID.setPID(kP, kI, kD, kF);
 		leftPID.setPID(kP, kI, kD, kF);
-		
+
 		double rightOut = rightPID.getOutput(getRightSpeed());
 		double leftOut = leftPID.getOutput(getLeftSpeed());
-		
+
 		setRightSpeed(rightOut);
 		setLeftSpeed(leftOut);
 	}
-	
+
 	/**
-	 * Set the desired velocity for the right motors (to make it move run updatePID() periodically) 
-	 * @param velocity velocity in METERS/SEC
+	 * Set the desired velocity for the right motors (to make it move run
+	 * updatePID() periodically)
+	 * 
+	 * @param velocity
+	 *            velocity in METERS/SEC
 	 */
-	public void setRightSetpoint(double velocity){
+	public void setRightSetpoint(double velocity) {
 		double error = velocity - getRightSpeed();
 		ntRightError.setDouble(error);
 		rightPID.setSetpoint(velocity);
 	}
-	
+
 	/**
-	 * Set the desired velocity for the left motors (to make it move run updatePID() periodically) 
-	 * @param velocity velocity in METERS/SEC
+	 * Set the desired velocity for the left motors (to make it move run
+	 * updatePID() periodically)
+	 * 
+	 * @param velocity
+	 *            velocity in METERS/SEC
 	 */
-	public void setLeftSetpoint(double velocity){
+	public void setLeftSetpoint(double velocity) {
 		double error = velocity - getLeftSpeed();
 		ntLeftError.setDouble(error);
 		leftPID.setSetpoint(velocity);
 	}
-	
+
 	/**
 	 * Set the speed of the two right motors
-	 * @param speed between -1 and 1
+	 * 
+	 * @param speed
+	 *            between -1 and 1
 	 */
 	public void setRightSpeed(double speed) {
-		driveRightRearMotor.set(speed);
-		driveRightFrontMotor.set(speed);
+		rightRearMotor.set(speed);
+		rightFrontMotor.set(speed);
 	}
-	
+
 	/**
 	 * Set the speed of the two left motors
-	 * @param speed between -1 and 1
+	 * 
+	 * @param speed
+	 *            between -1 and 1
 	 */
 	public void setLeftSpeed(double speed) {
-		driveLeftRearMotor.set(speed);
-		driveLeftFrontMotor.set(speed);
+		leftRearMotor.set(speed);
+		leftFrontMotor.set(speed);
 	}
-	
+
 	/**
 	 * Get the speed of the right wheels
+	 * 
 	 * @return speed in METER/SEC
 	 */
 	public double getRightSpeed() {
-		return driveRightEncoder.getRate();
+		return rightEncoder.getRate();
 	}
-	
+
 	/**
 	 * Get the speed of the left wheels
+	 * 
 	 * @return speed in METER/SEC
 	 */
 	public double getLeftSpeed() {
-		return driveLeftEncoder.getRate();
+		return leftEncoder.getRate();
 	}
-	
+
 	/**
-	 * Get the distance the right wheels have passed since the beginning of the program
+	 * Get the distance the right wheels have passed since the beginning of the
+	 * program
+	 * 
 	 * @return distance in METER
 	 */
 	public double getRightDistance() {
-		return driveRightEncoder.getDistance();
+		return rightEncoder.getDistance();
 	}
-	
+
 	/**
-	 * Get the distance the left wheels have passed since the beginning of the program
+	 * Get the distance the left wheels have passed since the beginning of the
+	 * program
+	 * 
 	 * @return distance in METER
 	 */
 	public double getLeftDistance() {
-		return driveLeftEncoder.getDistance();
+		return leftEncoder.getDistance();
 	}
-	
+
 	/**
 	 * 
-	 * @return true if the robot's on the cable bump on the center of the arena (in the null territory)
+	 * @return true if the robot's on the cable bump on the center of the arena
+	 *         (in the null territory)
 	 */
-	public boolean isBump(){
+	public boolean isBump() {
 		return bumpSensor.get();
 	}
-	
+
 	/**
 	 * Get the distance from the back of the robot <br>
 	 * <i>Note: Shows ~0.3 M under 0.3 M</i>
+	 * 
 	 * @return distance in METER
 	 */
-	public double getBackDistance(){
+	public double getBackDistance() {
 		return backDistanceSensor.getVoltage() * ultransonicMeterFactor;
 	}
 }
-
