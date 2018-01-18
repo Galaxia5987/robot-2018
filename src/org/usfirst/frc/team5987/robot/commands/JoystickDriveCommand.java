72220b88e152798fcab5c59b5d6e48b9f6a08e55
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-01-18 22:21:39 +0200

    Add drive command with joysticks

---
+++ src/org/usfirst/frc/team5987/robot/commands/JoystickDriveCommand.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team5987.robot.commands;
+
+import org.usfirst.frc.team5987.robot.OI;
+import org.usfirst.frc.team5987.robot.Robot;
+import org.usfirst.frc.team5987.robot.subsystems.DriveSubsystem;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class JoystickDriveCommand extends Command {
+
+	public JoystickDriveCommand() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.driveSubsystem);
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		DriveSubsystem.differentialDrive.tankDrive(OI.leftStick.getY(), OI.rightStick.getY());
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
+}
