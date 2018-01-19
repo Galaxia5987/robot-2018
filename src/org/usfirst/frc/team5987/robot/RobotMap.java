829e0dd2e4a94ef82411dbab3e2757fc8c44e8e8
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-01-19 16:07:22 +0200

    fix ports

--- src/org/usfirst/frc/team5987/robot/RobotMap.java
+++ src/org/usfirst/frc/team5987/robot/RobotMap.java
@@ -15,55 +15,70 @@ package org.usfirst.frc.team5987.robot;
  */
 public class RobotMap {
 	
-	/*--------------------------CLIMB-------------------------------*/
-	public static int climbRightServo = 8; // TODO: CHANGE!
-	public static int climbLeftServo = 9; // TODO: CHANGE!
+	/*--------------------------------MOTORS----------------------------------*/
 	
-	public static int climbMotor = 5; // TODO: CHANGE!
-	public static int climbLimitSwitch = 9; // TODO: CHANGE!
-	/*--------------------------------------------------------------*/
+					/*---------------CLIMB-----------------*/
+					public static int climbRightServo = 8; // TODO: CHANGE!
+					public static int climbLeftServo = 9; // TODO: CHANGE!
+					public static int climbMotor = 10; // TODO: CHANGE!
+					/*-------------------------------------*/
 
 	
-	/*--------------------------DRIVE-------------------------------*/
-	public static final int driveRightRearMotor = 10; // TODO: CHANGE!
-	public static final int driveRightFrontMotor = 11; // TODO: CHANGE!
-	public static final int driveLeftRearMotor = 12; // TODO: CHANGE!
-	public static final int driveLeftFrontMotor = 13; // TODO: CHANGE!
+					/*----------------DRIVE----------------*/
+					public static final int driveRightRearMotor = 4; // TODO: CHANGE!
+					public static final int driveRightFrontMotor = 5; // TODO: CHANGE!
+					public static final int driveLeftRearMotor = 6; // TODO: CHANGE!
+					public static final int driveLeftFrontMotor = 7; // TODO: CHANGE!
+					/*-------------------------------------*/
 	
-	public static final int driveRightEncoderChannelA = 2; // TODO: CHANGE!
-	public static final int driveRightEncoderChannelB = 3; // TODO: CHANGE!
-	public static final int driveLeftEncoderChannelA = 4; // TODO: CHANGE!
-	public static final int driveLeftEncoderChannelB = 5; // TODO: CHANGE!
+					/*---------------GRIPPER---------------*/
+					public static int gripperWheelLeft = 2; // TODO: CHANGE!
+					public static int gripperWheelRight = 3; // TODO: CHANGE!
+					/*-------------------------------------*/
+					
+					/*----------------LIFT-----------------*/
+					public static int liftMotorPort = 11;
+					/*-------------------------------------*/
 	
-	public static final int driveEncoderDistancePerPulse = 1 * 1; // TODO: CHANGE!
+					/*----------------INTAKE---------------*/
+					public static final int intakeMotorLeft = 0;
+					public static final int intakeMotorRight = 1;
+					/*-------------------------------------*/
 	
-	public static final int backUltrasonic = 0; // TODO: CHANGE!
-	public static int bumpSensor = 8; // TODO: CHANGE!
-	/*--------------------------------------------------------------*/
+	/*-----------------------------------SENSORS----------------------------------*/
 	
-	/*--------------------------GRIPPER------------------------------*/
-	public static int gripperWheelLeft = 2; // TODO: CHANGE!
-	public static int gripperWheelRight = 3; // TODO: CHANGE!
+					/*---------------CLIMB-----------------*/
+					public static int climbLimitSwitch = 6; // TODO: CHANGE!
+					/*-------------------------------------*/
+					
+					/*----------------DRIVE----------------*/
+					public static final int driveRightEncoderChannelA = 2; // TODO: CHANGE!
+					public static final int driveRightEncoderChannelB = 3; // TODO: CHANGE!
+					public static final int driveLeftEncoderChannelA = 4; // TODO: CHANGE!
+					public static final int driveLeftEncoderChannelB = 5; // TODO: CHANGE!
+					
+					public static final int driveEncoderDistancePerPulse = 1 * 1; // TODO: CHANGE!
+					
+					public static final int backUltrasonic = 0; // TODO: CHANGE!
+					public static int bumpSensor = 7; // TODO: CHANGE!
+					/*-------------------------------------*/
+					
+					/*---------------GRIPPER---------------*/
+					public static int proximityChannel = 1; // TODO: CHANGE!
+					/*-------------------------------------*/
+					
+					/*----------------LIFT-----------------*/
+					public static int liftEncoderPortA = 0;
+					public static int liftEncoderPortB = 1;
+					
+					public static int liftHallEffectTop = 8;
+					public static int liftHallEffectBottom = 9;
+					/*-------------------------------------*/
+					
+					/*----------------INTAKE---------------*/
+					public static final int intakeSolenoid1 = 6666;
+					public static final int intakeSolenoid2 = 66666;
+					/*-------------------------------------*/
 	
-	public static int proximityChannel = 1; // TODO: CHANGE!
-	/*--------------------------------------------------------------*/
-
-	/*--------------------------LIFT------------------------------*/
-	public static int liftMotorPort = 15;
-	
-	public static int liftEncoderPortA = 0;
-	public static int liftEncoderPortB = 11;
-	
-	public static int liftHallEffectTop = 9;
-	public static int liftHallEffectBottom = 8;
-	/*--------------------------------------------------------------*/
-	
-	/*--------------------------INTAKE------------------------------*/
-	public static final int intakeMotorLeft = 66;
-	public static final int intakeMotorRight = 666;
-	
-	public static final int intakeSolenoid1 = 6666;
-	public static final int intakeSolenoid2 = 66666;
-	/*--------------------------------------------------------------*/
-	
+	/*----------------------------------------------------------------------------*/
 }
