c85fa4bceb4f91cff3ad8ee2dd39b1d3c813f068
msmow <student@Robotics.k12.il>
2018-01-20 17:21:56 +0200

    created command

---
+++ src/org/usfirst/frc/team5987/robot/commands/DriveToBumpCommand.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team5987.robot.commands;
+
+import org.usfirst.frc.team5987.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveToBumpCommand extends Command {
+
+    public DriveToBumpCommand() {
+    	requires(Robot.driveSubsystem);
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+}
