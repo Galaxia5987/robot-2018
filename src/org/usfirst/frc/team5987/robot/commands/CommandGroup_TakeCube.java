c019a41faf11c5e5471da21a0cee9509f4df77bb
pauloalbert <student@Robotics.k12.il>
2018-01-21 18:57:29 +0200

    added driving forward

--- src/org/usfirst/frc/team5987/robot/commands/CommandGroup_TakeCube.java
+++ src/org/usfirst/frc/team5987/robot/commands/CommandGroup_TakeCube.java
@@ -3,16 +3,18 @@ package org.usfirst.frc.team5987.robot.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
- * 
+ * Will lower the intake and elevator, then drives forward and pulls in  the block
  * @author Paulo Khayat
  */
 public class CommandGroup_TakeCube extends CommandGroup {
 
     public CommandGroup_TakeCube() {
-    	addParallel(new IntakeSelenoidCommand(true));
-    	addSequential(new LiftCommand(LiftCommand.liftStates.BOTTOM));
-    	addParallel(new IntakeTakeCubeCommand());
-    	addParallel(new GripperTakeCubeCommand());
+    	
+    	addParallel(new IntakeSelenoidCommand(true)); //Lowers the intake
+    	addSequential(new LiftCommand(LiftCommand.liftStates.BOTTOM)); //lowers the lift
+    	addParallel(new IntakeTakeCubeCommand()); //turns the intake
+    	addParallel(new GripperTakeCubeCommand()); //turns the gripper
+    	addParallel(new DriveStraightCommand(0.5)); //TODO: change to camera OR a simple drive
     	// Add Commands here:
         // e.g. addSequential(new Command1());
         //      addSequential(new Command2());
