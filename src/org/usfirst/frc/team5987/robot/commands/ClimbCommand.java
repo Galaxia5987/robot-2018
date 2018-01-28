0c42bc5fcc64d79ef6fbfff703b06c7b99ba97a4
Dan Katzuv <student@Robotics.k12.il>
2018-01-28 20:23:12 +0200

    Add constructor for climb command which gets speed

--- src/org/usfirst/frc/team5987/robot/commands/ClimbCommand.java
+++ src/org/usfirst/frc/team5987/robot/commands/ClimbCommand.java
@@ -6,50 +6,72 @@ import edu.wpi.first.wpilibj.command.Command;
 
 /**
  * Command for climbing on the Rung.
+ * 
  * @author Dan Katzuv
  */
 public class ClimbCommand extends Command {
-	public final double climbSpeed = 1;
+
 	/**
-	 * Whether the lift should go up.
+	 * Whether the climbing mechanism should work.
 	 */
 	private boolean doesGoUp;
-    public ClimbCommand() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.climbSubsystem);
-    }
-    
-    public ClimbCommand(boolean turnOn) {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.climbSubsystem);
-    	this.doesGoUp = turnOn;
-    }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	/**
+	 * Speed climbing of the robot.
+	 */
+	private double climbSpeed;
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	double speed = doesGoUp ? climbSpeed : 0;
-    	Robot.climbSubsystem.setClimbSpeed(speed);
-    }
+	public ClimbCommand() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.climbSubsystem);
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-    	// finish if reached top or supposed to stop
-        return Robot.climbSubsystem.hasReachedTop() || !doesGoUp;
-    }
+	/**
+	 * Constructor
+	 * @param turnOn - Whether the climbing mechanism should work.
+	 */
+	public ClimbCommand(boolean turnOn) {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.climbSubsystem);
+		this.doesGoUp = turnOn;
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    	Robot.climbSubsystem.setClimbSpeed(0);
-    }
+	/**
+	 * Constructs the climb command.
+	 * @param turnOn - Whether the climbing mechanism should work.
+	 * @param climbSpeed - Climbing speed of the robot.
+	 */
+	public ClimbCommand(boolean turnOn, double climbSpeed) {
+		requires(Robot.climbSubsystem);
+		this.doesGoUp = turnOn;
+		this.climbSpeed = climbSpeed;
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		double speed = doesGoUp ? climbSpeed : 0;
+		Robot.climbSubsystem.setClimbSpeed(speed);
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		// finish if reached top or supposed to stop
+		return Robot.climbSubsystem.hasReachedTop() || !doesGoUp;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.climbSubsystem.setClimbSpeed(0);
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
