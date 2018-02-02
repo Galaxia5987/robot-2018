9f53f7493b965889b8e946f9aace4f83e222d39a
Unknown <dan.dan.kafe@gmail.com>
2018-02-02 17:00:11 +0200

    Add joysticks and correct imports

--- src/org/usfirst/frc/team5987/robot/OI.java
+++ src/org/usfirst/frc/team5987/robot/OI.java
@@ -7,18 +7,10 @@
 
 package org.usfirst.frc.team5987.robot;
 import org.usfirst.frc.team5987.robot.commands.ClimbCommand;
-
-
-import org.usfirst.frc.team5987.robot.commands.GripperShootCommand;
-import org.usfirst.frc.team5987.robot.commands.GripperTakeCubeCommand;
-
-import org.usfirst.frc.team5987.robot.commands.IntakeSelenoidCommand;
-import org.usfirst.frc.team5987.robot.commands.IntakeTakeCubeCommand;
-
 import org.usfirst.frc.team5987.robot.commands.DriveStraightCommand;
-
 import org.usfirst.frc.team5987.robot.commands.OpenHooksCommand;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
@@ -28,6 +20,8 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+	public Joystick leftStick = new Joystick(0);
+	public Joystick rightStick = new Joystick(1);
 	XboxController xbox = new XboxController(0);
 	Button a = new JoystickButton(xbox, 1);
 	Button b = new JoystickButton(xbox, 2);
