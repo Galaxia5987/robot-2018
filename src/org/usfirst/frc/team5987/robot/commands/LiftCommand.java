ecc0a10ea5d78733d812ebf05e363b085f3fea37
Dor Brekhman <dor46200@gmail.com>
2018-02-04 16:11:28 +0200

    Remove clearStickyFaults from lift command (now it's in the subsystem)

--- src/org/usfirst/frc/team5987/robot/commands/LiftCommand.java
+++ src/org/usfirst/frc/team5987/robot/commands/LiftCommand.java
@@ -55,7 +55,7 @@ public class LiftCommand extends Command {
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() { 
-		Robot.liftTalonSubsystem.clearStickyFaults();
+
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
