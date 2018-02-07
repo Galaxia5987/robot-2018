e7634c8df2fa9fca0b7b7f8c84a975e662fc7676
A Cool Name <acoolname9001@gmail.com>
2018-02-07 16:58:33 +0200

    added try except for removing crash on fully convex shapes
    Signed-off-by: A Cool Name <acoolname9001@gmail.com>

--- Kernel Operations/edge_detection.py
+++ Kernel Operations/edge_detection.py
@@ -12,7 +12,7 @@ kernel = np.ones((3, 3), dtype=np.uint8)
 mask = cv2.erode(mask, kernel=kernel, iterations=3)
 mask = cv2.dilate(mask, kernel=kernel, iterations=3)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img1 = img
+img1 = img.copy()
 laplacian = cv2.Laplacian(mask,cv2.CV_64F,ksize=25)
 _, thresh = cv2.threshold(laplacian,127,255, cv2.THRESH_BINARY)
 img2 = cv2.bitwise_and(thresh, 255)
@@ -22,13 +22,16 @@ cv2.drawContours(img1, contours, -1, (255, 0, 0), 3)
 for c in contours:
     hull = cv2.convexHull(c, returnPoints=False)
     defects = cv2.convexityDefects(c, hull)
-    for i in range(defects.shape[0]):
-        s, e, f, _ = defects[i, 0]
-        start = tuple(c[s][0])
-        end = tuple(c[e][0])
-        far = tuple(c[f][0])
-        cv2.line(img1, start, end, [0, 255, 0], 2)
-        cv2.circle(img1, far, 5, [0, 0, 255], -1)
+    try:
+        for i in range(defects.shape[0]):
+            s, e, f, _ = defects[i, 0]
+            start = tuple(c[s][0])
+            end = tuple(c[e][0])
+            far = tuple(c[f][0])
+            cv2.line(img1, start, end, [0, 255, 0], 2)
+            cv2.circle(img1, far, 5, [0, 0, 255], -1)
+    except AttributeError:
+        cv2.drawContours(img1,c,-1,(255,0,0),5)
 
 # sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)  # x
 # sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)  # y
