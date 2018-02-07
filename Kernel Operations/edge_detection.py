afc313158b85482387ed787be856397f6b283d94
msmow <masha.kondratieva@gmail.com>
2018-02-07 21:26:21 +0200

    did stuff

--- Kernel Operations/edge_detection.py
+++ Kernel Operations/edge_detection.py
@@ -12,23 +12,25 @@ kernel = np.ones((3, 3), dtype=np.uint8)
 mask = cv2.erode(mask, kernel=kernel, iterations=3)
 mask = cv2.dilate(mask, kernel=kernel, iterations=3)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img1 = img
+img1 = img0
 laplacian = cv2.Laplacian(mask,cv2.CV_64F,ksize=25)
 _, thresh = cv2.threshold(laplacian,127,255, cv2.THRESH_BINARY)
 img2 = cv2.bitwise_and(thresh, 255)
 img2 = np.array(img2, dtype=np.uint8)
 _, contours, _ = cv2.findContours(img2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-cv2.drawContours(img1, contours, -1, (255, 0, 0), 3)
-for c in contours:
-    hull = cv2.convexHull(c, returnPoints=False)
-    defects = cv2.convexityDefects(c, hull)
-    for i in range(defects.shape[0]):
-        s, e, f, _ = defects[i, 0]
-        start = tuple(c[s][0])
-        end = tuple(c[e][0])
-        far = tuple(c[f][0])
-        cv2.line(img1, start, end, [0, 255, 0], 2)
-        cv2.circle(img1, far, 5, [0, 0, 255], -1)
+# cv2.drawContours(img1, contours, -1, (255, 255, 255), 3)
+c = contours[0]
+hull = cv2.convexHull(c, returnPoints=False)
+defects = cv2.convexityDefects(c, hull)
+for i in range(defects.shape[0]):
+    s, e, f, _ = defects[i, 0]
+    start = tuple(c[s][0])
+    end = tuple(c[e][0])
+    far = tuple(c[f][0])
+    cv2.line(img1, start, end, [0, 255, 0], 2)
+    cv2.circle(img1, far, 5, [0, 0, 255], -1)
+x,y,w,h = cv2.boundingRect(c)
+cv2.rectangle(img1,(x,y),(x+w,y+h),(255,0,0),2)
 
 # sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)  # x
 # sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)  # y
