bf38ba8a4f04f20b4c7c08112c6b2ecb2087068a
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-02-09 19:13:39 +0200

    Add Watch_Doge to climb subsystem

--- src/org/usfirst/frc/team5987/robot/subsystems/ClimbSubsystem.java
+++ src/org/usfirst/frc/team5987/robot/subsystems/ClimbSubsystem.java
@@ -2,6 +2,8 @@ package org.usfirst.frc.team5987.robot.subsystems;
 
 import org.usfirst.frc.team5987.robot.RobotMap;
 import auxiliary.SafeVictorSPX;
+import auxiliary.Watch_Dogeable;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -9,7 +11,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * 
  * @author Dan Katzuv
  */
-public class ClimbSubsystem extends Subsystem {
+public class ClimbSubsystem extends Subsystem implements Watch_Dogeable {
 
 	public final boolean motor1Reversed = false;
 	public final boolean motor2Reversed = false;
@@ -25,6 +27,7 @@ public class ClimbSubsystem extends Subsystem {
 	SafeVictorSPX motor2 = new SafeVictorSPX(RobotMap.climbMotor2);
 	SafeVictorSPX motor3 = new SafeVictorSPX(RobotMap.climbMotor3);
 
+	Timer downTimer = new Timer();
 	
 	public ClimbSubsystem() {
 		motor1.setInverted(motor1Reversed);
@@ -47,8 +50,35 @@ public class ClimbSubsystem extends Subsystem {
 		motor3.set(speed);
 	}
 	
+	@Override
+	public void bork()
+	{
+		motor1.disable();
+		motor2.disable();
+		motor3.disable();
+		downTimer.reset();
+		downTimer.start();
 	}
 	
+	@Override
+	public void necromancy() {
+		motor1.enable();
+		motor2.enable();
+		motor3.enable();
 	}
+	
+	@Override
+	public boolean wakeMeUp() {
+		if (downTimer.get() >= 10) {
+			downTimer.stop();
+			downTimer.reset();
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public boolean ded() {
+		return motor1.status() && motor2.status() && motor3.status();
 	}
 }
