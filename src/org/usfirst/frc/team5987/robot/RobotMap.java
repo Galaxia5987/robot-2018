682f277f149ab246ee6d333e965bf5be9e7ea5dc
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-02-15 19:59:57 +0200

    Change ports to robot-b ports

--- src/org/usfirst/frc/team5987/robot/RobotMap.java
+++ src/org/usfirst/frc/team5987/robot/RobotMap.java
@@ -16,13 +16,11 @@ package org.usfirst.frc.team5987.robot;
 public class RobotMap {
 
 
-	/*---------------CLIMB-----------------*/
+	/*done---------------CLIMB-----------------*/
 
-	public static int climbRightServo = 10; // TODO: CHANGE!
-	public static int climbLeftServo = 11; // TODO: CHANGE!
 	
-	public static int climbMotor1 = 4;
-	public static int climbMotor2 = 5;
+	public static int climbMotor1 = 10;
+	public static int climbMotor2 = 9;
 	public static int climb1PDP = 12;
 	public static int climb2PDP = 13;
 	
@@ -31,72 +29,57 @@ public class RobotMap {
 
 
 
-	/*----------------DRIVE----------------*/
-	public static final int driveRightRearMotor = 1; // TODO: CHANGE!
-	public static final int driveRightFrontMotor = 2; // TODO: CHANGE!
-	public static final int driveLeftRearMotor = 3; // TODO: CHANGE!
-	public static final int driveLeftFrontMotor = 4; // TODO: CHANGE!
+	/*done----------------DRIVE----------------*/
+	public static final int driveRightRearMotor = 8; // TODO: CHANGE!
+	public static final int driveRightFrontMotor = 9; // TODO: CHANGE!
+	public static final int driveLeftRearMotor = 6; // TODO: CHANGE!
+	public static final int driveLeftFrontMotor = 7; // TODO: CHANGE!
 	/*-------------------------------------*/
 
-	/*---------------GRIPPER---------------*/
-	public static int gripperWheelLeft = 8; // TODO: CHANGE!
-	public static int gripperLeftPDP = 8; // TODO: CHANGE!
-	public static int gripperWheelRight = 6; // TODO: CHANGE!
-	public static int gripperRightPDP = 3; // TODO: CHANGE!
+	/*done---------------GRIPPER---------------*/
+	public static int gripperWheelLeft = 2; // TODO: CHANGE!
+	public static int gripperLeftPDP = 6; // TODO: CHANGE!
+	public static int gripperWheelRight = 3; // TODO: CHANGE!
+	public static int gripperRightPDP = 5; // TODO: CHANGE!
 	
 	public static final int[] gripperPDPs = new int[] {gripperLeftPDP, gripperRightPDP};
 	/*-------------------------------------*/
 
-	/*----------------LIFT-----------------*/
+	/*done----------------LIFT-----------------*/
 
 	public static int liftMotorPort = 2;
 
 	/*-------------------------------------*/
 
-	/*----------------INTAKE---------------*/
-	public static final int intakeMotorLeft = 7;
-	public static int intakeLeftPDP = 2;
+	/*done----------------INTAKE---------------*/
+	public static final int intakeMotorLeft = 4;
+	public static int intakeLeftPDP = 4;
 	public static final int intakeMotorRight = 5;
-	public static int intakeRightPDP = 1;
+	public static int intakeRightPDP = 11;
 	
 	public static final int[] intakePDPs = new int[] {intakeLeftPDP, intakeRightPDP};
 	/*-------------------------------------*/
 
 	/*-----------------------------------SENSORS----------------------------------*/
 
-	/*---------------CLIMB-----------------*/
-	public static int climbLimitSwitch = 6; // TODO: CHANGE!
-	/*-------------------------------------*/
+	/*done----------------DRIVE----------------*/
 
-	/*----------------DRIVE----------------*/
-
-	public static final int driveRightEncoderChannelA = 0; // TODO: CHANGE!
-	public static final int driveRightEncoderChannelB = 1; // TODO: CHANGE!
-	public static final int driveLeftEncoderChannelA = 2; // TODO: CHANGE!
-	public static final int driveLeftEncoderChannelB = 3; // TODO: CHANGE!
+	public static final int driveRightEncoderChannelA = 2; // TODO: CHANGE!
+	public static final int driveRightEncoderChannelB = 3; // TODO: CHANGE!
+	public static final int driveLeftEncoderChannelA = 0; // TODO: CHANGE!
+	public static final int driveLeftEncoderChannelB = 1; // TODO: CHANGE!
 
 	public static final double driveEncoderDistancePerPulse = 0.00133; // TODO:
 																	// CHANGE!
 
 	public static final int backUltrasonic = 1; // TODO: CHANGE!
-	public static int bumpSensor = 7; // TODO: CHANGE!
-	public static int colorSensor = 2;
 	/*-------------------------------------*/
 
-	/*---------------GRIPPER---------------*/
+	/*done---------------GRIPPER---------------*/
 	public static int proximityChannel = 0; // TODO: CHANGE!
 	/*-------------------------------------*/
-
-	/*----------------LIFT-----------------*/
-	public static int liftEncoderPortA = 4;
-	public static int liftEncoderPortB = 5;
-
-	public static int liftHallEffectTop = 8;
-	public static int liftHallEffectBottom = 9;
-	/*-------------------------------------*/
-
 	
-	/*----------------INTAKE---------------*/
+	/*done----------------INTAKE---------------*/
 	public static final int intakeSolenoid1 = 0;
 	public static final int intakeSolenoid2 = 1;
 	/*-------------------------------------*/
