021e646f62e53e9344eeec9ff3427ffdbaef2002
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-02-20 17:05:25 +0200

    Fix Switch constructor
    Co-Authored-By: dor-b <dor-b@users.noreply.github.com>

--- src/org/usfirst/frc/team5987/robot/commands/autos/Switch.java
+++ src/org/usfirst/frc/team5987/robot/commands/autos/Switch.java
@@ -17,45 +17,16 @@ import org.usfirst.frc.team5987.robot.commands.ChangeFilterModeCommand.Modes;
  *
  */
 public class Switch extends CommandGroup {
-	private char startPosition;
 	
-	public Switch(char startPosition) {
-		this.startPosition = startPosition;
+	public Switch() {
 	
-		/**
-		 * The game specific message which includes data about the Scale and
-		 * Switches Plates randomanization.
-		 */
-		String gameData = DriverStation.getInstance().getGameSpecificMessage();
-
 		// Positions of the alliance Switch and Scale Plates.
-		char switchPosition = gameData.charAt(0), scalePosition = gameData.charAt(1);
+		char switchPosition = DriverStation.getInstance().getGameSpecificMessage().charAt(0);
 
-		NetworkTable autoTable = NetworkTableInstance.getDefault().getTable("Autonomous");
-
-		/**
-		 * Distance from the alliance wall to the auto line. REMEMBER TO ADD A
-		 * FACTOR.
-		 */
-		NetworkTableEntry autoLineDistance = autoTable.getEntry("auto line distance");
-
-		/************************* SWITCH ********************/
-		/**
-		 * Distance the robot drives forward when heading to the switch,
-		 * starting at the aliance wall.
-		 */
-		NetworkTableEntry beginningSwitchDist = autoTable.getEntry("switch distance 1");
-
-		/**
-		 * Angle the robot rotates to when heading to the switch (between
-		 * {@link #switchDist1} and {@link #distFromSwitch}).
-		 */
-		NetworkTableEntry switchAngle = autoTable.getEntry("switch angle");
 		addSequential(new ChangeFilterModeCommand(ChangeFilterModeCommand.Modes.SWITCH));
 		addSequential(new IntakeSolenoidCommand(true));
 		// Robot is in center, ready to go to one of two Platforms of the
 		// Switch.
-		if (startPosition == 'C') {
 			addSequential(new DriveStraightCommand(Constants.AUTO_SWITCH_STRAIGHT));
 			// Switch plate is on the left.
 			if (switchPosition == 'L') {
@@ -72,12 +43,5 @@ public class Switch extends CommandGroup {
 
 			addSequential(new ShootCubeCommand(1, true));
 			addSequential(new ChangeFilterModeCommand(ChangeFilterModeCommand.Modes.STREAM));
-		}
-
-		if (startPosition != scalePosition) {
-			addSequential(new DriveStraightCommand(autoLineDistance.getDouble(0)));
-		}
-//		addSequential(ArriveToScaleGroupCommand()); // TODO: Merge branch of arriving to the Scale and add here.
-//		addSequential(PutCubeOnScaleGroupCommand()); // TODO: Merge branch of putting a Power Cube on the Scale and add here.
 	}
 }
