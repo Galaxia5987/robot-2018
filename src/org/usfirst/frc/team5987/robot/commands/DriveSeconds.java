46212e602c30d26ab8b6184d62e6618e4362c35b
believeInU <doron12109@gmail.com>
2018-03-10 12:50:35 +0200

    Add DriveSeconds command that doesn't use the encoders

---
+++ src/org/usfirst/frc/team5987/robot/commands/DriveSeconds.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team5987.robot.commands;
+
+import org.usfirst.frc.team5987.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveSeconds extends Command {
+	
+    private double speed;
+
+	public DriveSeconds(double speed, double seconds) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	this.speed = speed;
+    	setTimeout(seconds);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.driveSubsystem.setLeftSpeed(speed);
+    	Robot.driveSubsystem.setRightSpeed(speed);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return isTimedOut();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.driveSubsystem.setLeftSpeed(0);
+    	Robot.driveSubsystem.setRightSpeed(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
