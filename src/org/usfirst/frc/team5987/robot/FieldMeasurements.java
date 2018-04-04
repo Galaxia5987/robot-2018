161fd6270b2b06bda55e9646f0ce2d6ebd8191c5
believeInU <student@Robotics.k12.il>
2018-04-04 16:55:24 +0300

    Add FieldMeasurements class for storing positions

---
+++ src/org/usfirst/frc/team5987/robot/FieldMeasurements.java
@@ -0,0 +1,73 @@
+package org.usfirst.frc.team5987.robot;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import auxiliary.Point;
+/**
+ * Measurements from right close corners in METERS
+ * @author student
+ *
+ */
+public class FieldMeasurements {
+	public Point getRelative(Point robotStartingPosition, Point destination){
+		return destination.minus(robotStartingPosition);
+	}
+	
+	public enum Cube {
+		/*
+		 * From close to far, then right to left (1 is the closest and
+		 * rightmost)
+		 */
+		PLATFORM_1, PLATFORM_2, PLATFORM_3, PLATFORM_4, PLATFORM_5, PLATFORM_6, 
+		PILE_1, PILE_2, PILE_3, PILE_4, PILE_5, PILE_6
+	}
+
+	public enum Switch {
+		TARGET_RIGHT, TARGET_LEFT
+	}
+
+	public enum Vault {
+		VAULT
+	}
+	
+	public enum Robot {
+		LEFT, RIGHT, CENTER
+	}
+
+	public static Map<Cube, Point> CUBES = new EnumMap<Cube, Point>(Cube.class);
+	static {
+		CUBES.put(Cube.PLATFORM_1, new Point(0, 0));
+		CUBES.put(Cube.PLATFORM_2, new Point(0, 0));
+		CUBES.put(Cube.PLATFORM_3, new Point(0, 0));
+		CUBES.put(Cube.PLATFORM_4, new Point(0, 0));
+		CUBES.put(Cube.PLATFORM_5, new Point(0, 0));
+		CUBES.put(Cube.PLATFORM_6, new Point(0, 0));
+
+		CUBES.put(Cube.PILE_1, new Point(0, 0));
+		CUBES.put(Cube.PILE_2, new Point(0, 0));
+		CUBES.put(Cube.PILE_3, new Point(0, 0));
+		CUBES.put(Cube.PILE_4, new Point(0, 0));
+		CUBES.put(Cube.PILE_5, new Point(0, 0));
+		CUBES.put(Cube.PILE_6, new Point(0, 0));
+
+	}
+	
+	public static Map<Switch, Point> SWITCHES = new EnumMap<Switch, Point>(Switch.class);
+	static {
+		SWITCHES.put(Switch.TARGET_RIGHT, new Point(0, 0));
+		SWITCHES.put(Switch.TARGET_LEFT, new Point(0, 0));
+	}
+	
+	public static Map<Vault, Point> VAULTS = new EnumMap<Vault, Point>(Vault.class);
+	static {
+		VAULTS.put(Vault.VAULT, new Point(0, 0));
+	}
+	
+	public static Map<Robot, Point> ROBOTS = new EnumMap<Robot, Point>(Robot.class);
+	static {
+		ROBOTS.put(Robot.RIGHT, new Point(0, 0));
+		ROBOTS.put(Robot.LEFT, new Point(0, 0));
+		ROBOTS.put(Robot.CENTER, new Point(0, 0));
+	}
+}
