c11b4467f8408c91aaaefe4ce5ed441093f22c22
msmow <masha.kondratieva@gmail.com>
2018-04-04 15:39:40 +0300

    started fixing edge detection

---
+++ Vision/temporary/edge_detection.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+
+#cam = cv2.VideoCapture(1)
+while True:
+    #_, img = cam.read()
+    img = cv2.imread("cube.jpg")
+    #img = np.ndarray(img)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    ranges = [((21, 147, 65), (38, 255, 255)), ((14, 0, 227), (129, 108, 255)), ((16, 103, 52), (94, 255, 255))]
+    masks = []
+
+    kernel = (
+        (0,1,0),
+        (1,1,1),
+        (0,1,0)
+    )
+    kernel=np.array(kernel,dtype=np.uint8)
+
+    for r in ranges:
+        masks.append(cv2.inRange(hsv, r[0], r[1]))
+
+    mask = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
+
+    for m in masks:
+        mask = cv2.bitwise_or(mask, m)
+
+    mask = cv2.dilate(mask, kernel=kernel, iterations=5)
+    mask = cv2.erode(mask, kernel=kernel, iterations=10)
+    mask = cv2.dilate(mask, kernel=kernel, iterations=5)
+
+    img1 = img.copy()
+    img2 = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img2 = cv2.GaussianBlur(img2, ksize=(7, 7), sigmaX=0)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_HSV2BGR)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    img3 = cv2.bitwise_and(mask, img2)
+
+    canny = cv2.Canny(img3, 50, 75)
+    _, thresh = cv2.threshold(canny, 10, 255, cv2.THRESH_BINARY)
+    thresh = np.array(thresh, dtype=np.uint8)
+    thresh = cv2.dilate(thresh, kernel=kernel, iterations=5)
+    thresh = cv2.erode(thresh, kernel=kernel, iterations=3)
+    mask = cv2.bitwise_and(cv2.bitwise_not(thresh), mask)
+
+    _, contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(img1, contours, -1, [255, 0, 0], 3)
+
+    for c in contours:
+        hull=cv2.convexHull(c)
+        epsilon = 0.015*cv2.arcLength(hull,True)
+        approx = cv2.approxPolyDP(hull,epsilon,True)
+        hullpoints = cv2.convexHull(approx,returnPoints=True)
+        #if len(hullpoints) == 6 or len(hullpoints) == 5:
+        for i in range(0,len(hullpoints)):
+            cv2.line(img1, tuple(hullpoints[i-1][0]), tuple(hullpoints[i][0]), [0, 255, 0], 2)
+            cv2.circle(img1, tuple(hullpoints[i][0]), 5, [0, 0, 255], -1)
+
+    cv2.imshow("thresh", thresh)
+    cv2.imshow("mask", mask)
+    cv2.imshow("image", img1)
+    key = cv2.waitKey(1)
+    if key is ord('q'):
+        break
+
+
---
+++ Vision/temporary/manual_range_finder.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# USAGE: You need to specify a filter and "only one" image source
+#
+# (python) range-detector --filter RGB --image /path/to/image.png
+# or
+# (python) range-detector --filter HSV --webcam
+
+import cv2
+import argparse
+from operator import xor
+
+
+def callback(value):
+    pass
+
+
+def setup_trackbars(range_filter):
+    cv2.namedWindow("Trackbars", 0)
+
+    for i in ["MIN", "MAX"]:
+        v = 0 if i == "MIN" else 255
+
+        for j in range_filter:
+            cv2.createTrackbar("%s_%s" % (j, i), "Trackbars", v, 255, callback)
+
+
+def get_arguments():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-f', '--filter', required=True,
+                    help='Range filter. RGB or HSV')
+    ap.add_argument('-i', '--image', required=False,
+                    help='Path to the image')
+    ap.add_argument('-w', '--webcam', required=False,
+                    help='Use webcam', action='store_true')
+    ap.add_argument('-p', '--preview', required=False,
+                    help='Show a preview of the image after applying the mask',
+                    action='store_true')
+    args = vars(ap.parse_args())
+    args = vars(ap.parse_args())
+
+    if not xor(bool(args['image']), bool(args['webcam'])):
+        ap.error("Please specify only one image source")
+
+    if not args['filter'].upper() in ['RGB', 'HSV']:
+        ap.error("Please speciy a correct filter.")
+
+    return args
+
+
+def get_trackbar_values(range_filter):
+    values = []
+
+    for i in ["MIN", "MAX"]:
+        for j in range_filter:
+            v = cv2.getTrackbarPos("%s_%s" % (j, i), "Trackbars")
+            values.append(v)
+
+    return values
+
+
+def main():
+    #camera = cv2.VideoCapture(1)
+    #camera.set(cv2.CAP_PROP_SETTINGS,1)
+    setup_trackbars('HSV')
+
+    while True:
+
+        #ret, image = camera.read()
+        ret = True
+        image = cv2.imread("cubes.jpg")
+
+        if not ret:
+            break
+
+
+        frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = get_trackbar_values("HSV")
+
+        thresh = cv2.inRange(frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))
+
+        cv2.imshow("Original", image)
+        cv2.imshow("Thresh", thresh)
+        key=cv2.waitKey(1)
+        if key is ord("q"):
+            break
+        if key is not -1:
+            file=open("../files/Colors_"+str(key-48)+".val","w+")
+            file.write("self.lower_range,self.upper_range = ({},{},{}),({},{},{})".format(v1_min, v2_min, v3_min, v1_max, v2_max, v3_max))
+            file.close()
+if __name__ == '__main__':
+    main()
